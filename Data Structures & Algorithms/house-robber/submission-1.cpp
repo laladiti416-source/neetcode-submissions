576264756cb37bd999784cecab1df1c5d9c1e938
laladiti416-source <laladiti416@gmail.com>
2026-06-22 23:39:33 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size()-1;
+        vector<int> dp(n+2,-1);
+        dp[n] = nums[n];
+        dp[n+1] = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            dp[i] = max((nums[i] + dp[i+2]),dp[i+1]);
+        }
+        return dp[0];
+    }
+};
